bb43dbfa7589848ef12fa3a7b3fcd32e5f4caf71
guillermo varela <guillovarela16@gmail.com>
2019-06-10 19:20:17 -0300

    seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'clearing database'
+Article.destroy_all
+puts 'creating articles'
+Article.create!(title: 'hola como andas todo bien',
+ photo1: 'https://www.infobae.com/new-resizer/_YyWx8ah6FEGUmNElLWPlp9TkAc=/999x526/filters:quality(100)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2019/06/10183826/diacono-degollado-1.jpg'
+ photo2: 'https://www.infobae.com/new-resizer/i-AA46CheRGnQX1NseGs2_TXpr8=/999x526/filters:quality(100)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2019/06/10141550/Mauricio-Macri-Ivan-Duque-1.jpg',
+ subtitle: 'hola como andas todo bien y bos tus cosas estan muy bien?')
+puts 'finished'
